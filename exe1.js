9df9070a75d6c4008b25363c2b5a8a5c058792c8
radhadpi1 <radha.dpi@gmail.com>
2023-10-28 11:32:36 -0400

    exe1

---
+++ exe1.js
@@ -0,0 +1,11 @@
+let radius = 3
+//let radius = 4
+ radius = 4
+const pi = 3.14
+ var radius1 = 5
+ var radius1 = 6
+radius1 = 7
+area = pi*radius*radius
+area1 = pi* radius1 *radius1
+console.log('area: ' + area)
+console.log('area1 :'+area1)
